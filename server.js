721f182ae66a350af74b041bd04a93b4d0e5ffdc
Melvin Hernandez <melher39@gmail.com>
2019-04-13 00:50:01 -0500

    added code to be able to deploy to heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require("./routes");
 // initialize Express
 const app = express();
 // API PORT for local use, PORT 3000 will be for client (react app)
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Use morgan logger for logging http requests
 // this logger is actually very useful
@@ -15,6 +15,11 @@ app.use(logger("dev"));
 // setup express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// code to properly deploy (to heroku in our case)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 
 // give express access to our routes
 app.use(routes);
